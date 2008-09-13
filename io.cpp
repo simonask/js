7e43c25c88b480188666fa77578be811531c98d9
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-09-13 12:46:07 +0200

    Declared JS-methods static.

--- io.cpp
+++ io.cpp
@@ -10,14 +10,14 @@ static Persistent<FunctionTemplate> io_templ;
 
 enum InternalFields { FILE_HANDLE = 0, NUM_FIELDS };
 
-Handle<Value> wrap_file_pointer(FILE* fp)
+static Handle<Value> wrap_file_pointer(FILE* fp)
 {
 	Local<Object> io = io_templ->GetFunction()->NewInstance();
 	io->SetInternalField(FILE_HANDLE, External::New((void*)fp));
 	return io;
 }
 
-FILE* unwrap_file_pointer(Handle<Object> self)
+static FILE* unwrap_file_pointer(Handle<Object> self)
 {
 	Local<Value> field = self->GetInternalField(FILE_HANDLE);
 	return reinterpret_cast<FILE*>(Handle<External>::Cast(field)->Value());
@@ -26,7 +26,7 @@ FILE* unwrap_file_pointer(Handle<Object> self)
 /*
 	METHODS
 */
-Handle<Value> js_io_new(const Arguments& args)
+static Handle<Value> js_io_new(const Arguments& args)
 {
 	if (args.Length() < 2 && args.Length() != 0)
 		assert(0 && "not enough arguments for js_io_new!"); // TODO: Throw exception
@@ -35,7 +35,7 @@ Handle<Value> js_io_new(const Arguments& args)
 	return args.This();
 }
 
-Handle<Value> js_io_open(const Arguments& args)
+static Handle<Value> js_io_open(const Arguments& args)
 {
 	if (args.Length() < 1)
 		assert(0 && "not enough arguments for js_io_open!"); // TODO: Throw exception
@@ -48,14 +48,14 @@ Handle<Value> js_io_open(const Arguments& args)
 		return wrap_file_pointer(fp);
 }
 
-Handle<Value> js_io_close(const Arguments& args)
+static Handle<Value> js_io_close(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	fclose(fp);
 	return True();
 }
 
-Handle<Value> js_io_read(const Arguments& args)
+static Handle<Value> js_io_read(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	size_t n = args[0]->ToInteger()->Value();
@@ -64,7 +64,7 @@ Handle<Value> js_io_read(const Arguments& args)
 	return String::New(buf, m);
 }
 
-Handle<Value> js_io_write(const Arguments& args)
+static Handle<Value> js_io_write(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	const char* data = *String::Utf8Value(args[0]);
@@ -72,13 +72,13 @@ Handle<Value> js_io_write(const Arguments& args)
 	return Integer::New(n);
 }
 
-Handle<Value> js_io_flush(const Arguments& args)
+static Handle<Value> js_io_flush(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	return fflush(fp) ? False() : True();
 }
 
-Handle<Value> js_io_seek(const Arguments& args)
+static Handle<Value> js_io_seek(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	int offset = args[0]->ToInteger()->Value();
@@ -86,7 +86,7 @@ Handle<Value> js_io_seek(const Arguments& args)
 	return fseek(fp, offset, whence) ? False() : True();
 }
 
-Handle<Value> js_io_tell(const Arguments& args)
+static Handle<Value> js_io_tell(const Arguments& args)
 {
 	FILE* fp = unwrap_file_pointer(args.This());
 	return Integer::New(ftell(fp));
@@ -96,19 +96,19 @@ Handle<Value> js_io_tell(const Arguments& args)
 	ACCESSORS
 */
 
-Handle<Value> js_io_handle(Local<String> property, const AccessorInfo& info)
+static Handle<Value> js_io_handle(Local<String> property, const AccessorInfo& info)
 {
 	return Integer::New((int64_t)unwrap_file_pointer(info.Holder()));
 }
 
-Handle<Value> js_io_eof(Local<String> property, const AccessorInfo& info)
+static Handle<Value> js_io_eof(Local<String> property, const AccessorInfo& info)
 {
 	FILE* fp = unwrap_file_pointer(info.Holder());
 	return feof(fp) ? True() : False();
 }
 
 
-void dummy_setter(Local<String>, const Local<Value>, const AccessorInfo&) {}
+static void dummy_setter(Local<String>, const Local<Value>, const AccessorInfo&) {}
 
 void IO::initialize(Handle<Object>& global)
 {
